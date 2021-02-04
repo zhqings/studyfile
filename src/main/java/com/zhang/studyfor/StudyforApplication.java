463ce25dac06c32e46160aff495c1c4eb64da250
haiqing.zhang <298080741@qq.com>
2021-02-04 17:30:16 +0800

    first commit

---
+++ src/main/java/com/zhang/studyfor/StudyforApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.studyfor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyforApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyforApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhang/studyfor/StudyforApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhang.studyfor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyforApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
